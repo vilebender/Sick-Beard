5cc68760ba9de29437e5d9f08a4e91920ffe6492
Maciej Kulinski <vilebender@gmail.com>
2013-09-08 13:23:52 +0100

    Changed update repo to "vilebender".

--- sickbeard/versionChecker.py
+++ sickbeard/versionChecker.py
@@ -114,7 +114,7 @@ class CheckVersion():
 class UpdateManager():
 
     def get_github_repo_user(self):
-        return 'midgetspy'
+        return 'vilebender'
 
     def get_github_repo(self):
         return 'Sick-Beard'
